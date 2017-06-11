7cae65de22639e6f8c67d84b3d1e8cce28ae21f6
vollmerr <vollmerr@oregonstate.edu>
2017-06-11 00:01:55 -0700

    manual tests, invalid tests

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -24,13 +24,30 @@ public class UrlValidatorTest {
     private boolean printStatus = false;
     private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
 
-    public UrlValidatorTest(String testName) {
-    }
+    public UrlValidatorTest(String testName) {}
 
-
+    /**
+     * Tests various specific cases manually
+     */
     public void testManualTest() {
         UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-        System.out.println(urlVal.isValid("http://www.amazon.com"));
+
+        printHeader("isValid manually");
+
+        System.out.println(String.format("Testing valid urls..."));
+        testIsValid("http://foo.com/bar", "", "", true);
+        testIsValid("https://foo.com/bar", "", "", true);
+        testIsValid("file:///foo", "", "", true);
+        testIsValid("http://foo.ru/bar", "", "", true);
+        testIsValid("http://foo.com?q=bar", "", "", true);
+        testIsValid("http://foo.com:8080", "", "", true);
+        System.out.println(String.format("Done testing valid urls.\n"));
+
+        System.out.println(String.format("Testing valid urls..."));
+        testIsValid("http://123.456.789.000", "", "", false);
+        testIsValid("http:/foo.com", "", "", false);
+        testIsValid("http://foo.com/../", "", "", false);
+        System.out.println(String.format("Done testing valid urls.\n"));
     }
 
     /**
@@ -42,6 +59,7 @@ public class UrlValidatorTest {
         String google = "www.google.com";
         String url = http + google;
 
+        printHeader("isValid with valid URLs");
         testSection(validSchemes, "Valid Scheme", none, google, true);
         testSection(validHosts, "Valid Host", http, none, true);
         testSection(validPorts, "Valid Port", url, none, true);
@@ -49,34 +67,54 @@ public class UrlValidatorTest {
         testSection(validQuerys, "Valid Query", url, none, true);
     }
 
+    /**
+     * Tests all invalid sections of url
+     */
     public void testYourSecondPartition() {
+        String none = "";
+        String http = "http://";
+        String google = "www.google.com";
+        String url = http + google;
 
+        printHeader("isValid with invalid URLs");
+        testSection(invalidSchemes, "Invalid Scheme", none, google, false);
+        testSection(invalidHosts, "Invalid Host", http, none, false);
+        testSection(invalidPorts, "Invalid Port", url, none, false);
+        testSection(invalidPaths, "Invalid Path", url, none, false);
+        testSection(invalidQuerys, "Invalid Query", url, none, false);
     }
 
     public void testIsValid(String url, String type, String value, boolean expected) {
         UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
         boolean actual = urlVal.isValid(url);
+        String message = url;
+
         if (actual != expected) {
+            if (type != "") {
+                message = String.format("%s with %s of '%s'", url, type, value);
+            }
             System.out.println("\tFAILED: ");
-            System.out.println(String.format("\t\tExpected %s with %s of '%s' to be %s, instead got %s.", url, type, value, expected, actual));
+            System.out.println(String.format("\t\tExpected %s to be %s, instead got %s.", message, expected, actual));
         }
         else {
             System.out.println("\t.");
         }
     }
 
-    public void testAnyOtherUnitTest() {
-
-    }
+    public void testAnyOtherUnitTest() {}
 
     public void testSection(String[] values, String type, String preText, String postText, boolean expected) {
         String url;
 
-        System.out.println(String.format("Testing %s's.", type));
+        System.out.println(String.format("Testing %s's...", type));
         for (int i = 0; i < values.length; i++) {
             testIsValid(preText + values[i] + postText, type, values[i], expected);
         }
-        System.out.println(String.format("Done testing %s's\n", type));
+        System.out.println(String.format("Done testing %s's.\n", type));
+    }
+
+    public void printHeader(String type) {
+        System.out.println(String.format("\n******* Testing %s *******\n", type));
     }
 
     public static void main(String[] argv) {
@@ -91,19 +129,24 @@ public class UrlValidatorTest {
     String[] invalidSchemes = {
             "://",
             "$",
-            "http:/"
+            "http:/",
+            "123"
     };
     String[] validSchemes = {
             "",
             "aaa://",
             "http://",
-            "z39.50s://"
+            "z39.50s://",
+            "file:///"
     };
 
     String[] invalidHosts = {
             "0.0.0",
             "0.0.0.256",
-            "test.invalid"
+            "@#$%",
+            "-1.0.0.0",
+            "test.invalid",
+            ""
     };
     String[] validHosts = {
             "www.google.com",
@@ -118,23 +161,26 @@ public class UrlValidatorTest {
             "-1",
             ":65536",
             ":a",
-            ""
+            "123"
     };
     String[] validPorts = {
             ":0",
             ":80",
-            ":65535"
+            ":65535",
+            ""
     };
 
     String[] invalidPaths = {
             "/../",
-            "///"
+            "///",
+            "abc",
+            "123"
     };
     String[] validPaths = {
-            "/test",
+            "/test/test2/",
             "/$123",
             "/",
-            ""
+            "#anchor"
     };
 
     String[] invalidQuerys = {};
